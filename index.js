44c6852b1725a2091d05c6fce88507953d1a1aae
Daniel Rezende <danielrezende2@academico.ufs.br>
2024-04-03 15:31:50 -0300

    added env support

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+const Client = require("pg").Client;
+
+const client = new Client({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+client.connect();
+console.log("Connected to Postgres");
+client.end();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
