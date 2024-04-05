fb77193c603fe439b268c48d5fb00e971f418632
Daniel Rezende <danielrezende2@academico.ufs.br>
2024-04-05 18:59:21 -0300

    adicionado swagger para sabores

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # 2023.2-bd-parte4-etapa2
 Um app que contém 3 tabelas (2 entidades e 1 relacionamento), contendo as operações CRUD, assim como também uma operação de transação.
 
+## Como rodar o app?
+
+Primeiro instale todos as dependências necessárias
+
+```
+npm install
+```
+
+Depois para começar o programa, utilize este comando
+
+```
+npm install
+```
 ## Lista de afazeres
 
 - [x] Criar 3 tabelas de banco de dados
     - [x] 2 entidades
     - [x] 1 relacionamento ou agregação
 - [ ] Operações CRUD
-    - [ ] Insert
-    - [ ] Update
-    - [ ] Read
-    - [ ] Delete
+    - [x] sabores
+    - [ ] pedidos
+    - [ ] sabores_pedidos
 - [ ] Implementar um método transação, ou seja, faz uma pesquisa e depois insere algo na tabela
 - [ ] Implementar um swagger
+    - [x] sabores
+    - [ ] pedidos
+    - [ ] sabores_pedidos
--- routes/sabores.js
+++ routes/sabores.js
@@ -12,30 +12,198 @@ const pool = new Pool({
 pool.connect();
 
 const sabores = require("express").Router();
-
+/**
+ * @swagger
+ * /sabores:
+ *   get:
+ *     summary: Recupera uma lista de sabores
+ *     description: Recupera uma lista de sabores do banco de dados.
+ *     responses:
+ *       200:
+ *         description: Uma lista de sabores.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: O ID do sabor.
+ *                   nome:
+ *                     type: string
+ *                     description: O nome do sabor.
+ *                   descricao:
+ *                     type: string
+ *                     description: A descrição do sabor.
+ *                   tipo:
+ *                     type: string
+ *                     description: O tipo do sabor.
+ *                   vegano:
+ *                     type: boolean
+ *                     description: Se o sabor é vegano ou não.
+ *                   sem_lactose:
+ *                     type: boolean
+ *                     description: Se o sabor é sem lactose ou não.
+ *                   preco:
+ *                     type: number
+ *                     description: O preço do sabor.
+ *       500:
+ *         description: Erro ao dar SELECT em todos os sabores devido ao servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao dar SELECT em todos os sabores.
+ *                   example: Erro com servidor!
+ */
 sabores.get("/", async (req, res) => {
-  await pool.query("SELECT * FROM sabores;", (error, results) => {
-    if (error) {
-      throw error;
-    }
+  try {
+    await pool.query("SELECT * FROM sabores;");
     res.status(200).send(results.rows);
-  });
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+/**
+ * @swagger
+ * /sabores/{id}:
+ *   get:
+ *     summary: Recupera um sabor pelo ID
+ *     description: Recupera um sabor específico do banco de dados usando o ID do sabor.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do sabor a ser recuperado.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Um sabor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: O ID do sabor.
+ *                 nome:
+ *                   type: string
+ *                   description: O nome do sabor.
+ *                 descricao:
+ *                   type: string
+ *                   description: A descrição do sabor.
+ *                 tipo:
+ *                   type: string
+ *                   description: O tipo do sabor.
+ *                 vegano:
+ *                   type: boolean
+ *                   description: Se o sabor é vegano ou não.
+ *                 sem_lactose:
+ *                   type: boolean
+ *                   description: Se o sabor é sem lactose ou não.
+ *                 preco:
+ *                   type: number
+ *                   description: O preço do sabor.
+ *       500:
+ *         description: Erro ao dar SELECT no id do sabor devido ao servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao dar SELECT no id do sabor.
+ *                   example: Erro com servidor!
+ */
 sabores.get("/:id", async (req, res) => {
   const { id } = req.params;
-  await pool.query(
-    "SELECT * FROM sabores WHERE id = $1",
-    [id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.status(200).send(results.rows);
-    }
-  );
+  try {
+    await pool.query("SELECT * FROM sabores WHERE id = $1", [id]);
+    res.status(200).send(results.rows);
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+/**
+ * @swagger
+ * /sabores:
+ *   post:
+ *     summary: Adiciona um novo sabor
+ *     description: Adiciona um novo sabor ao banco de dados.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *                 description: O nome do sabor.
+ *               descricao:
+ *                 type: string
+ *                 description: A descrição do sabor.
+ *               tipo:
+ *                 type: string
+ *                 description: O tipo do sabor.
+ *               vegano:
+ *                 type: boolean
+ *                 description: Se o sabor é vegano ou não.
+ *               sem_lactose:
+ *                 type: boolean
+ *                 description: Se o sabor é sem lactose ou não.
+ *               preco:
+ *                 type: number
+ *                 description: O preço do sabor.
+ *     responses:
+ *       200:
+ *         description: Sabor adicionado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que o sabor foi adicionado com sucesso.
+ *                   example: Sabor adicionado com sucesso!
+ *       400:
+ *         description: Erro ao adicionar sabor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao adicionar o sabor.
+ *                   example: Falta alguns parametros para adicionar a tabela!
+ *       500:
+ *         description: Erro ao adicionar sabor devido ao servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao adicionar o sabor.
+ *                   example: Erro com servidor!
+ */
 sabores.post("/", async (req, res) => {
   const requiredParams = [
     "nome",
@@ -58,21 +226,151 @@ sabores.post("/", async (req, res) => {
     const query =
       "INSERT INTO sabores (nome, descricao, tipo, vegano, sem_lactose, preco) VALUES ($1, $2, $3, $4, $5, $6);";
     const values = requiredParams.map((param) => values_query[param]);
-    const result = await pool.query(query, values);
-    res.json({ message: "Sabor adicionado com sucesso!" });
+    try {
+      const result = await pool.query(query, values);
+      res.json({ message: "Sabor adicionado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: "Erro com servidor!" });
+    }
   }
 });
 
+/**
+ * @swagger
+ * /sabores/{id}:
+ *   delete:
+ *     summary: Deleta um sabor pelo ID
+ *     description: Deleta um sabor específico do banco de dados usando o ID do sabor.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do sabor a ser deletado.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Sabor deletado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que o sabor foi deletado com sucesso.
+ *                   example: Sabor deletado com sucesso!
+ *       400:
+ *         description: Erro ao deletar sabor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao deletar o sabor.
+ *                   example: Erro ao deletar sabor!
+ *       500:
+ *         description: Erro ao deletar sabor devido ao servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao deletar o sabor.
+ *                   example: Erro com servidor!
+ */
 sabores.delete("/:id", async (req, res) => {
   const query = "DELETE FROM sabores WHERE id = $1;";
-  const result = await pool.query(query, [req.params.id]);
-  if (result.rowCount === 0) {
-    res.status(500).json({ message: "Erro ao deletar sabor!" });
-  } else {
-    res.json({ message: "Sabor deletado com sucesso!" });
+  try {
+    await pool.query(query, [req.params.id]);
+    if (result.rowCount === 0) {
+      res.status(400).json({ message: "Erro ao deletar sabor!" });
+    } else {
+      res.json({ message: "Sabor deletado com sucesso!" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erro com servidor!" });
   }
 });
 
+/**
+ * @swagger
+ * /sabores:
+ *   put:
+ *     summary: Atualiza um sabor existente
+ *     description: Atualiza um sabor existente no banco de dados usando o ID do sabor.
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         required: true
+ *         description: ID do sabor a ser atualizado.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *                 description: O nome do sabor.
+ *               descricao:
+ *                 type: string
+ *                 description: A descrição do sabor.
+ *               tipo:
+ *                 type: string
+ *                 description: O tipo do sabor.
+ *               vegano:
+ *                 type: boolean
+ *                 description: Se o sabor é vegano ou não.
+ *               sem_lactose:
+ *                 type: boolean
+ *                 description: Se o sabor é sem lactose ou não.
+ *               preco:
+ *                 type: number
+ *                 description: O preço do sabor.
+ *     responses:
+ *       200:
+ *         description: Sabor atualizado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que o sabor foi atualizado com sucesso.
+ *                   example: Sabor atualizado com sucesso!
+ *       400:
+ *         description: Erro ao atualizar sabor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro ao atualizar o sabor.
+ *                   example: No fields to update or Missing required parameter id
+ *       500:
+ *         description: Erro ao atualizar sabor devido ao servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Uma mensagem indicando que ocorreu um erro com o servidor ao atualizar o sabor.
+ *                   example: Erro com servidor!
+ */
 sabores.put("/", async (req, res) => {
   const { id, ...fields } = req.query;
 
@@ -101,7 +399,7 @@ sabores.put("/", async (req, res) => {
     res.json({ message: "Sabor atualizado com sucesso!" });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: "Error updating sabor" });
+    res.status(500).json({ message: "Erro com servidor!" });
   }
 });
 
